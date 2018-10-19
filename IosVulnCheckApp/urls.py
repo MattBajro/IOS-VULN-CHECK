5c4f93e865c604afad553c39e59b2393cda86fd6
MattBajro <ceeert@gmail.com>
2018-10-19 06:51:16 -0700

    Change URL mappings in urls.py files to satisfy deployment on test server

--- IosVulnCheckApp/urls.py
+++ IosVulnCheckApp/urls.py
@@ -4,7 +4,7 @@ from django.views.generic.base import RedirectView
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.index, name='index'),
+    url(r'^$', views.FilteredDeviceListView.as_view(), name='index'),
     url(r'^device-table$', views.FilteredDeviceListView.as_view(), name='device-table'),
     url(r'^vuln-table$', views.FilteredVulnListView.as_view(), name='vuln-table'),
     url(r'^device-detail/(?P<device_id>[\S ]+)$', views.device_details, name='device-details'),
--- mysite/urls.py
+++ mysite/urls.py
@@ -17,6 +17,6 @@ from django.conf.urls import include, url
 from django.contrib import admin
 
 urlpatterns = [
-    url(r'^vulnapp/', include('IosVulnCheckApp.urls')),
     url(r'^admin/', admin.site.urls),
+    url(r'', include('IosVulnCheckApp.urls')),
 ]
